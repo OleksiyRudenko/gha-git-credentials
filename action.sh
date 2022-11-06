e7a10d8587d4d9706f13965a1442e329f5be53d7
OleksiyRudenko <oleksiy.rudenko++git@gmail.com>
2022-11-06 19:50:17 +0100

    add(action.sh): add versioning warning

--- action.sh
+++ action.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 set -e
 
+__warning_title="Not the latest version of oleksiyrudenko/gha-git-credentials action in use"
+__warning="
+This workflow uses oleksiyrudenko/gha-git-credentials@v2.1
+
+It is recommended to use oleksiyrudenko/gha-git-credentials@v2-latest to have the latest backward compatible version of the action within the scope of v2.
+
+See https://github.com/OleksiyRudenko/gha-git-credentials/blob/master/CHANGELOG.md for the most recent updates.
+"
+
+echo "::warning title=$__warning_title::$__warning"
+
 INPUT_GLOBAL=${INPUT_GLOBAL:-false}
 _GIT_GLOBAL_OPTION=''
 if [ "$INPUT_GLOBAL" = true ] ; then
